b09dbd1761f5d04023dad049116fbab992b908ea
HouleyD-oumar <oumar.pro_sch@outlook.fr>
2025-09-29 15:14:48 +0200

    ajout de la fonction de soustraction de deux nombres

--- calcul.js
+++ calcul.js
@@ -1,2 +1,4 @@
 const calculateSum = (FirstNumber, SecondNumber) => FirstNumber + SecondNumber;
+const calculateSubstract = (FirstNumber, SecondNumber) => FirstNumber - SecondNumber;
 console.log(calculateSum(5, 10));
+console.log(calculateSubstract(5, 10));
