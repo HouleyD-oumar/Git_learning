ca3fdba53c20b087787070ffffc6c5bb27892575
HouleyD-oumar <oumar.pro_sch@outlook.fr>
2025-09-29 15:42:35 +0200

    changement des variables de test de la fonction

--- calcul.js
+++ calcul.js
@@ -1,4 +1,4 @@
 const calculateSum = (FirstNumber, SecondNumber) => FirstNumber + SecondNumber;
 const calculateSubstract = (FirstNumber, SecondNumber) => FirstNumber - SecondNumber;
-console.log(calculateSum(5, 10));
+console.log(calculateSum(100, 90));
 console.log(calculateSubstract(5, 10));
